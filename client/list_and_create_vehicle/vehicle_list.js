39cbc5a9221e597e6080a39ebb0a76ad0f364344
Brian Schroer <schroer.brian@gmail.com>
2013-11-17 14:34:17 -0800

    first iteration of create+list sidebar styling and design

--- client/list_and_create_vehicle/vehicle_list.js
+++ client/list_and_create_vehicle/vehicle_list.js
@@ -17,8 +17,26 @@ Template.vehicle_list_item.selected = function () {
     return Session.equals("selected", this._id) ? "active" : '';
 };
 
+Template.vehicle_list.events({
+   
+   'click input#vl-add-moto': function (e) {
+      e.preventDefault();
+      $(".vl-add-moto-wrap").fadeOut();
+      $(".vehicle_create_dialog").fadeIn();
+    }
+
+});
+
 Template.vehicle_list_item.events({
     'click': function(e) {
         Session.set("selected", this._id);
     }
+
 });
+
+// 'click a#mileage_update_link': function (e) {
+//     e.preventDefault();
+//     $("a#mileage_update_link").hide();
+//     $("div#vehicle_details").append(Meteor.render(
+//         Template.mileage_update_event_form));
+// }
